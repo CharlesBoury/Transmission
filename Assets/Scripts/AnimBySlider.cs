9611188ad67e3f31a280875b6ea7d6ecb8eb0f80
CharlesBoury <boury.charles@gmail.com>
2018-01-28 23:00:23 +0100

    Ambiance de la journée

---
+++ Assets/Scripts/AnimBySlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimBySlider : MonoBehaviour {
+
+	public Slider mainSlider;
+	Animator animator;
+	public string nomDeLanim;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+		animator.Play(nomDeLanim, -1, mainSlider.value);
+	}
+}
