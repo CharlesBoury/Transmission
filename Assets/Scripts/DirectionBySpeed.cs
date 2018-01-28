81d9d79b95e3f7aa7876844642acce123cee46a6
CharlesBoury <boury.charles@gmail.com>
2018-01-28 21:31:07 +0100

    Anim marche

--- Assets/Scripts/DirectionBySpeed.cs
+++ Assets/Scripts/DirectionBySpeed.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class DirectionBySpeed : MonoBehaviour {
 
 	Vector3 previousPos;
-	public Transform tete;
-	public Transform corps;
+	public Transform bulle;
+	public Animator animator;
 
 	void Start() {
 		previousPos = Vector3.zero;
+		animator = GetComponent<Animator>();
 	}
 
 	void Update () {
 		Vector3 delta = transform.position - previousPos;
 
+		if (delta.x == 0) {
+			animator.SetBool("marche", false);
+		} else animator.SetBool("marche", true);
 		if (delta.x > 0) {
-			tete.localScale = new Vector3(-1,1,1);
-			corps.localScale = new Vector3(-1,1,1);
+			transform.localScale = new Vector3(1,1,1);
+			bulle.localScale = new Vector3(1,1,1);
 		}
 		else if (delta.x < 0) {
-			tete.localScale = new Vector3(1,1,1);
-			corps.localScale = new Vector3(1,1,1);
+			transform.localScale = new Vector3(-1,1,1);
+			bulle.localScale = new Vector3(-1,1,1);
 		}
 
 		previousPos = transform.position;
